9ee632ef7d74ad175b0d76af780711030c721e07
drhakihub <drhaki@outlook.com>
2025-12-21 12:56:14 +0100

    create sample landing page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,13 @@
-import Link from 'next/link';
+import Footers02 from '../components/creative-tim/blocks/footers-02';
+import Hero01 from '../components/creative-tim/blocks/hero-01';
+import SimpleFeaturesBlockWithImage from '../components/creative-tim/blocks/simple-features-block-with-image';
 
 export default function HomePage() {
   return (
     <div className="flex flex-col justify-center text-center flex-1">
-      <h1 className="text-2xl font-bold mb-4">Hello World</h1>
-      <p>
-        You can open{' '}
-        <Link href="/docs" className="font-medium underline">
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+      <Hero01 />
+      <SimpleFeaturesBlockWithImage />
+      <Footers02 />
     </div>
   );
 }
