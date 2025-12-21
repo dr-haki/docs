8074ea988e6e3703c96a3f5b987a2499772cd205
drhakihub <drhaki@outlook.com>
2025-12-21 13:03:52 +0100

    install sass and create scss folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
 import './global.css';
+import './styles/main.scss'
 import { Inter } from 'next/font/google';
 import { dmSans, spaceMono } from './fonts';
 
